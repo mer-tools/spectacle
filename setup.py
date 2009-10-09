e52b825039727231f8f4f19292fa9e7a21756504
Anas Nashif <anas.nashif@intel.com>
2009-10-09 08:59:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='spectcle',
+      version='0.1',
+      description='Spectacle',
+      author='Anas Nashif',
+      author_email='anas.nashif@intel.com',
+      url='http://moblin.org/',
+      scripts='spectacle',
+      packages=['distutils', 'distutils.command'],
+     )
+
