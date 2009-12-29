1314d4aef587c53c516f95f79eb543a74c7e7a15
JF Ding <Jian-feng.Ding@intel.com>
2009-12-29 17:55:01 +0800

    Add version tracking in spectacle package

--- setup.py
+++ setup.py
@@ -2,8 +2,23 @@
 
 from distutils.core import setup
 
+version = 'unknown'
+try:
+    import spectacle.__version__
+    version = spectacle.__version__.version
+except:
+    version_path = 'VERSION'
+    try:
+        # first line should be the version number
+        version = open(version_path).readline().strip()
+        ver_file = open('spectacle/__version__.py', 'w')
+        ver_file.write("version = \"%s\"\n" % version)
+        ver_file.close()
+    except:
+        pass
+
 setup(name='spectacle',
-      version='0.5',
+      version = version,
       description='Spectacle',
       author='Anas Nashif, Jian-feng Ding',
       author_email='anas.nashif@intel.com, jian-feng.ding@intel.com',
