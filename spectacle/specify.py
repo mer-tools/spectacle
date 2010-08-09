13cd96fc8b8b84034549357c62ef1b81b6064819
JF Ding <jian-feng.ding@intel.com>
2010-08-09 13:59:37 +0800

    specify: checking duplicate cleanup in install_post

--- spectacle/specify.py
+++ spectacle/specify.py
@@ -1253,6 +1253,12 @@ PkgBR:
         for key in content['files']:
             self._check_dup_files(content['files'][key])
 
+        # checking duplicate 'rm -rf %{buildroot}'
+        re_cleanup = re.compile('(rm|\%\{__rm\})\W+-rf\W+(\$RPM_BUILD_ROOT|\%\{buildroot\})')
+        if 'install' in content and 'post' in content['install']:
+            if re_cleanup.search(content['install']['post'][0]):
+                logger.warning('duplicate buildroot cleanup found in the first line of install_post, remove it')
+
         return content
 
     def process(self, extra_content):
