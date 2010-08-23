cf7562eafc8baac9d0512628eb218b61c4ed1367
JF Ding <jian-feng.ding@intel.com>
2010-08-23 15:55:16 +0800

    pump version to 0.19 for new release
    all dist/* files were removed, should not track them in git

--- dist/spectacle.dsc
+++
@@ -1,8 +0,0 @@
-Format: 1.0
-Source: spectacle
-Version: 0.18
-Binary: spectacle
-Maintainer: Jian-feng Ding <jian-feng.ding@intel.com>
-Architecture: all
-Standards-Version: 3.7.1
-Build-Depends: debhelper (>= 4.0.0), python-dev, python-cheetah
--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ try:
 except ImportError:
     pass
 
-if 'MAKEFLAGS' not in os.environ:
-    repl = raw_input('WARNING: Please use `make install` for installation, continue(y/N)? ')
-    if repl != 'y':
-        sys.exit(1)
+if 'install' in sys.argv and \
+   'MAKEFLAGS' not in os.environ and \
+   'RPM_BUILD_ROOT' not in os.environ:
+        repl = raw_input('WARNING: Please use `make install` for installation, continue(y/N)? ')
+        if repl != 'y':
+            sys.exit(1)
 
 # For debian based systems, '--install-layout=deb' is needed after 2.6
 if sys.version_info[:2] <= (2, 5) and '--install-layout=deb' in sys.argv:
